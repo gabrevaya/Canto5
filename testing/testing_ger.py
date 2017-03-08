7aa3163414052ce9770b995d07a8bec41891077d
Ger <germanabrevaya@gmail.com>
2017-03-07 21:57:01 -0300

    Envolvente funcionando en testing_ger.py

--- testing/testing_ger.py
+++ testing/testing_ger.py
@@ -28,24 +28,22 @@ times = np.arange(0,t_fin,1/sample_rate)
 #peaks = np.zeros(len(audio))
 #peaks[pospeakind] = audio[pospeakind]
 
-bin_size = 10
+bin_size = 100
 num_of_bins = int(data_points/bin_size)
 new_len = num_of_bins*bin_size
 
-segmented = np.reshape(audio[:new_len],[bin_size, num_of_bins])
-maxs = np.max(segmented, axis = 0)
+segmented = np.reshape(audio[:new_len],[num_of_bins,bin_size])
+maxs = np.max(segmented, axis = 1)
 t_start = int((bin_size*0.5))-1
 t_new = times[t_start:new_len:bin_size]
 
 tck = interpolate.splrep(t_new, maxs, s=0)
 envolvente = interpolate.splev(times, tck, der=0)
 
-# NO FUNCIONA LO DE LA ENVOLVENTE POR AHORA
-
 plt.figure(figsize=(30,10))
 plt.plot(times,audio)
 #plt.plot(t_new,maxs)
-plt.plot(times, envolvente)
+plt.plot(times, envolvente, linewidth=3)
 plt.xlabel('tiempo (s)')
 plt.ylabel('amplitud')
 #plt.plot(times,peaks,'.')
