b351b244fb17507a209804e89e705bcfa06296e4
apa <apa@apa.com>
2017-03-09 23:20:57 -0300

    Añadida función para encontrar las ventanas temporales donde se identifican las sílabas en el archivo de audio. La salida es una señal booleana.

---
+++ find_syllables.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+ 
+from read_wav import read_wav
+from envolvente import envolvente
+import numpy as np
+import matplotlib.pyplot as plt
+
+#audio_file="call1.wav"
+#[raw_audio, times, sample_rate, data_points]=read_wav(audio_file)
+#[env, t_env]=envolvente(raw_audio, times, sample_rate, data_points, 1000, 1, 0)
+
+def find_syllables(raw_audio, times, env, t_env, npoints_umbral, fbird) 
+   '''
+   Finds the time windows where the syllables are, inside the raw-audio file.
+   
+   Input: raw_audio, times - Amplitud and time vector of the original audio file 
+                             (only to plot a explanary graf).
+          env, t_env       - Amplitud and time vector of the wrapping wave of the  
+                             audio signal.
+          npoints_umbral   - Number of points counted from the start, considered
+                             to estimate a threshold value "umbral" that the 
+                             amplitud of the syllable usually surpasses.
+   Output: loc_silaba      - Boolean signal identifying the time windows where the 
+                             syllables are. (mask)
+                             
+   '''
+
+    #npoints_umbral=100
+    
+    umbral=np.max(env[:npoints_umbral])
+
+    #print ("umbral=",umbral)
+    
+    fbird=1.8
+
+    loc_silabas=np.greater(env,umbral*fbird)
+
+    #indices=np.nonzero(loc_silabas[1:] != loc_silabas[:-1])[0] + 1
+    #silabas=np.split(raw_audio,indices)
+
+
+    plt.plot(times,raw_audio)
+    plt.plot(t_env, env, linewidth=3)
+    plt.plot(t_env,loc_silabas*10000)
+    plt.xlabel('tiempo (s)')
+    plt.ylabel('amplitud')
+
+    plt.show()
+
+
+    #loc_index=np.diff(loc_silabas)
+    #plt.plot(loc_index)
+    #plt.show()
+
+    return loc_silabas
