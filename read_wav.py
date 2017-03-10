7f2405cf4117c2738920bddcfbe91ba9ca659a25
apa <apa@apa.com>
2017-03-09 23:23:18 -0300

    Comentada línea de código en la función read_wav que grafica señal en un tamaño específico (no se ajusta bien a diferentes pantallas).

--- read_wav.py
+++ read_wav.py
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 -*-
 
-
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.io.wavfile as sw
 
-def read_wav(file):
+def read_wav(audio_file):
     
     '''
     Read wav files.
@@ -15,15 +14,15 @@ def read_wav(file):
     samples.
     '''    
     
-    [sample_rate, raw_audio] = sw.read(file)
+    [sample_rate, raw_audio] = sw.read(audio_file)
     raw_audio = np.array(raw_audio)
     n_samples= len(raw_audio)
     t_fin = n_samples/sample_rate
     times = np.arange(0,t_fin,1/sample_rate)
     
-#    plt.figure(figsize=(30,10))
-#    plt.plot(times,raw_audio)
-#    plt.xlabel('tiempo (s)')
-#    plt.ylabel('amplitud')
+    #plt.figure(figsize=(30,10))
+    plt.plot(times,raw_audio)
+    plt.xlabel('tiempo (s)')
+    plt.ylabel('amplitud')
     
     return raw_audio, times, sample_rate, n_samples
