9eb1aac5e9ab147425223aae5d5efec54b4104fe
apa <apa@apa.com>
2017-03-10 16:41:32 -0300

    Corregidos algunos bugs menores

--- find_syllables.py
+++ find_syllables.py
@@ -9,21 +9,21 @@ import matplotlib.pyplot as plt
 #[raw_audio, times, sample_rate, data_points]=read_wav(audio_file)
 #[env, t_env]=envolvente(raw_audio, times, sample_rate, data_points, 1000, 1, 0)
 
-def find_syllables(raw_audio, times, env, t_env, npoints_umbral, fbird) 
-   '''
-   Finds the time windows where the syllables are, inside the raw-audio file.
+def find_syllables(raw_audio, times, env, t_env, npoints_umbral, fbird): 
+    '''
+    Finds the time windows where the syllables are, inside the raw-audio file.
    
-   Input: raw_audio, times - Amplitud and time vector of the original audio file 
-                             (only to plot a explanary graf).
-          env, t_env       - Amplitud and time vector of the wrapping wave of the  
-                             audio signal.
-          npoints_umbral   - Number of points counted from the start, considered
-                             to estimate a threshold value "umbral" that the 
-                             amplitud of the syllable usually surpasses.
-   Output: loc_silaba      - Boolean signal identifying the time windows where the 
-                             syllables are. (mask)
+    Input: raw_audio, times - Amplitud and time vector of the original audio file 
+                              (only to plot a explanary graf).
+           env, t_env       - Amplitud and time vector of the wrapping wave of the  
+                              audio signal.
+           npoints_umbral   - Number of points counted from the start, considered
+                              to estimate a threshold value "umbral" that the 
+                              amplitud of the syllable usually surpasses.
+    Output: loc_silaba      - Boolean signal identifying the time windows where the 
+                              syllables are. (mask)
                              
-   '''
+    '''
 
     #npoints_umbral=100
     
@@ -46,7 +46,7 @@ def find_syllables(raw_audio, times, env, t_env, npoints_umbral, fbird)
     plt.ylabel('amplitud')
 
     plt.show()
-    plt.save('loc_silabas.png')
+    #fig.savefig('loc_silabas.png')
     plt.close()
 
     #loc_index=np.diff(loc_silabas)
