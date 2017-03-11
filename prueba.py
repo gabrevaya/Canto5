26bd4153a60186d83b4e5a1c3cd697728d8d2dea
apa <apa@apa.com>
2017-03-11 09:12:04 -0300

    Añadido script de prueba para exposicion

---
+++ prueba.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+from main_func import main
+
+main('./datos_filtrados/albanella/1',1000,1.8)
+
+main('./datos_filtrados/albanella/2',500,0.5)
+
+#main('./datos_filtrados/Erithacus rubecula_Robin',300,2)
